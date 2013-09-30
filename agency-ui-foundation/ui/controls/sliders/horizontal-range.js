08361368469f9adf3a1aa3ab8e14c4208a093b89
Aubrey Taylor <ataylor@blitzagency.com>
2013-09-30 15:02:27 -0700

    Added horizontal sliders
    - Ported updated sliders from bigview
    - Updated require references to match config

---
+++ agency-ui-foundation/ui/controls/sliders/horizontal-range.js
@@ -0,0 +1,72 @@
+define(function(require, exports, module){
+
+    var Marionette = require('vendor/marionette'),
+        _ = require('vendor/underscore');
+        HorizontalSlider = require('auf/ui/controls/sliders/horizontal');
+
+
+    return Marionette.Controller.extend({
+
+        initialize: function(options){
+            _.bindAll(this, 'responderWantsMove');
+            this.slider = new HorizontalSlider(options);
+            this.slider.responderWantsMove = this.responderWantsMove;
+            this.listenTo(this.slider, 'change', this.rangeDidChange);
+        },
+
+        rangeDidChange: function(slider){
+            // forward the event
+            this.trigger('change', this);
+        },
+
+        responderWantsMove: function(responder){
+            var $h = responder.$el;
+
+            var action;
+            var handleIndex = this.slider.getHandleIndex($h);
+            var position = this.slider.calculatePositionXForDelta($h, responder.deltaX());
+
+            if(this.slider.steps){
+                var step = this.slider.calculateStepForPositionX($h, position);
+                var currentStep = this.slider.handleSteps[handleIndex];
+
+                if(step != currentStep){
+                    step = this.enforceRangeStep(handleIndex, step);
+                    this.slider.setStep($h, step);
+                }
+            } else {
+                position = this.enforceRangePosition(handleIndex, position);
+                this.slider.setPercent($h, position);
+            }
+        },
+
+        enforceRangePosition: function(index, position){
+            if(index == 1){
+                return Math.max(this.slider.handlePositionX[0], position);
+            }
+
+            return Math.min(this.slider.handlePositionX[1], position);
+        },
+
+        enforceRangeStep: function(index, step){
+            if(index == 1){
+                return Math.max(this.slider.handleSteps[0], step);
+            }
+
+            return Math.min(this.slider.handleSteps[1], step);
+        },
+
+
+        getPosition: function(){
+            return this.slider.handlePositionX;
+        },
+
+        getSteps: function(){
+            return this.slider.handleSteps;
+        },
+
+        onClose: function(){
+            this.slider.close();
+        }
+    });
+});
---
+++ agency-ui-foundation/ui/controls/sliders/horizontal.js
@@ -0,0 +1,254 @@
+define(function(require, exports, module){
+
+    var Marionette = require('vendor/marionette'),
+        _ = require('vendor/underscore');
+        mouse = require('auf/ui/responders/mouse');
+        touches = require('auf/ui/responders/touches');
+        OrientationResponder = require('auf/ui/responders/orientation').OrientationResponder;
+
+    return Marionette.Controller.extend({
+        $trackView: null,
+        steps: 0,
+
+        initialize: function(options){
+            var filtered = _.pick(options, '$trackView', 'steps');
+            _.extend(this, filtered);
+            _.bindAll(this, 'updateHandleOffset');
+
+            // normalizeTrack needs to happen 1st
+            this.normalizeTrackForAvailableHandles();
+            this.initializeSteps();
+
+            this.handles = [];
+            this.handleValues = [];
+            this.handleOffsetX = [];
+            this.handlePositionX = [];
+            this.handleTracks = [];
+            this.handleSteps = [];
+            this.mouseResponders = [];
+            this.touchResponders = [];
+
+            this.initializeHandles.apply(this, options.$handles);
+        },
+
+        initializeHandles: function(){
+            var process = _.bind(function($h){
+                var position = $h.position();
+                this.handles.push($h[0]);
+                this.handleValues.push($h);
+                this.handleTracks.push(this.normalizeTrack($h));
+                this.handleOffsetX.push(position.left);
+
+                var percentPosition = this.calculatePositionXForDelta($h, 0);
+                this.handlePositionX.push(percentPosition);
+
+                var step = this.calculateStepForPositionX($h, percentPosition);
+                this.handleSteps.push(step);
+
+            }, this);
+
+            _.each(arguments, process);
+
+            this.initializeTouches();
+            this.initializeMouse();
+            this.initializeOrientation();
+        },
+
+        initializeOrientation: function(){
+            var handleOrientation = _.bind(function(responder, e){
+                this.normalizeTrackForAvailableHandles();
+                this.initializeSteps();
+
+                var self = this;
+
+                _.each(this.handleValues, function($h){
+                    var handleIndex = self.getHandleIndex($h);
+                    var currentPosition = self.handlePositionX[handleIndex];
+                    self.setPercent($h, currentPosition);
+                    self.updateHandleOffset($h);
+                });
+            }, this);
+
+
+            this.orientationResponder = new OrientationResponder({
+                portrait: handleOrientation,
+                landscape: handleOrientation
+            });
+        },
+
+        initializeTouches: function(){
+            var touchStart = _.bind(function(responder, e){
+                 e.preventDefault();
+                 var $h = responder.$el;
+                 $h.css({'z-index': 1});
+            }, this);
+
+            var touchMove = _.bind(function(responder, e){
+                e.preventDefault();
+                this.responderWantsMove(responder);
+            }, this);
+
+            var touchEnd = _.bind(function(responder, e){
+                var $h = responder.$el;
+                $h.css({'z-index': 0});
+                this.updateHandleOffset($h);
+            }, this);
+
+            var process = _.bind(function($h){
+                var responder = new touches.TouchResponder({
+                    el: $h,
+                    touchStart: touchStart,
+                    touchMove: touchMove,
+                    touchEnd: touchEnd
+                });
+
+                this.touchResponders.push(responder);
+
+            }, this);
+
+            _.each(this.handleValues, process);
+        },
+
+        initializeMouse: function(){
+            var mouseDragged = _.bind(function(responder, e){
+                e.preventDefault();
+                this.responderWantsMove(responder);
+            }, this);
+
+            var mouseDown = _.bind(function(responder, e){
+                e.preventDefault();
+                var $h = responder.$el;
+                $h.css({'z-index': 1});
+
+            }, this);
+
+            var mouseUp = _.bind(function(responder, e){
+                var $h = responder.$el;
+                $h.css({'z-index': 0});
+                this.updateHandleOffset($h);
+            }, this);
+
+            var process = _.bind(function($h){
+                var responder = new mouse.MouseResponder({
+                    el: $h,
+                    mouseDragged: mouseDragged,
+                    mouseDown: mouseDown,
+                    mouseUp: mouseUp
+                });
+
+                this.mouseResponders.push(responder);
+
+            }, this);
+
+            _.each(this.handleValues, process);
+
+        },
+
+        initializeSteps: function (){
+            if(this.steps){
+                this.stepUnit = 1 / this.steps;
+            } else {
+                this.stepUnit = 0;
+            }
+        },
+
+        updateHandleOffset: function($h){
+            var handleIndex = this.getHandleIndex($h);
+            this.handleOffsetX[handleIndex] = $h.position().left;
+        },
+
+        getHandleIndex: function($h){
+            return this.handles.indexOf($h[0]);
+        },
+
+        getHandleOffsetX: function($h){
+            var handleIndex = this.getHandleIndex($h);
+            return this.handleOffsetX[handleIndex];
+        },
+
+        responderWantsMove: function(responder){
+            var $h = responder.$el;
+            var position = this.calculatePositionXForDelta($h, responder.deltaX());
+
+            if(this.steps){
+                var handleIndex = this.getHandleIndex($h);
+                var step = this.calculateStepForPositionX($h, position);
+                var currentStep = this.handleSteps[handleIndex];
+
+                if(step != currentStep){
+                    this.setStep($h, step);
+                }
+            } else {
+
+                this.setPercent($h, position);
+            }
+        },
+
+        calculateStepForPositionX: function($h, positionX){
+            var handleIndex = this.getHandleIndex($h);
+            var currentPosition = this.handlePositionX[handleIndex];
+            var action = positionX < currentPosition ? Math.ceil : Math.floor;
+            return action(positionX * this.steps);
+        },
+
+        calculatePositionXForDelta: function($h, deltaX){
+            var handleIndex = this.getHandleIndex($h);
+            var offsetX = this.getHandleOffsetX($h);
+            var normalizedTrack = this.handleTracks[handleIndex];
+            var posX = offsetX + deltaX;
+            var position = posX / normalizedTrack;
+
+            position = Math.min(1, position);
+            position = Math.max(0, position);
+
+            return position;
+        },
+
+        normalizeTrackForAvailableHandles: function(){
+            this.handleTracks = [];
+            var process = _.bind(function($h){
+                this.handleTracks.push(this.normalizeTrack($h));
+            }, this);
+
+            _.each(this.handleValues, process);
+        },
+
+        normalizeTrack: function($h){
+            var $track = this.$trackView;
+            var trackRect = $track[0].getBoundingClientRect();
+            var handleRect = $h[0].getBoundingClientRect();
+            return trackRect.width - handleRect.width;
+        },
+
+        setPercent: function ($h, percent){
+            var handleIndex = this.getHandleIndex($h);
+            var normalizedTrack = this.handleTracks[handleIndex];
+            var x = percent * normalizedTrack;
+            this.moveTo($h, x);
+            this.handlePositionX[handleIndex] = percent;
+            this.trigger('change', this);
+        },
+
+        setStep: function($h, value){
+            var handleIndex = this.getHandleIndex($h);
+            var target = Math.min(this.steps, parseInt(value, 10));
+            target = Math.max(0, target);
+
+            this.handleSteps[handleIndex] = target;
+            this.setPercent($h, target * this.stepUnit);
+        },
+
+        moveTo: function($el, posX){
+            $el.css({left: posX});
+        },
+
+        getPosition: function(){
+            return this.handlePositionX;
+        },
+
+        getSteps: function(){
+            return this.handleSteps;
+        }
+
+    });
+});
