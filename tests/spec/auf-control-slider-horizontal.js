a5782946a29cda0bd2a7da556029e2151e1ed508
Aubrey Taylor <ataylor@blitzagency.com>
2013-10-01 12:13:52 -0700

    Added test stubs for slider controls.

---
+++ tests/spec/auf-control-slider-horizontal.js
@@ -0,0 +1,19 @@
+define(function(require, exports, module) {
+
+describe('Control: Horizontal Range Slider', function() {
+
+    // Set Up
+    // ==================================================================== //
+
+    // Helpers
+    // ==================================================================== //
+
+    // Test Suite
+    // ==================================================================== //
+
+    it('example test function', function(){
+        // Do something here.
+    });
+
+}); // eof describe
+}); // eof define
