4152aa892a8997b5a543b3345b12f87c49446fad
Aubrey Taylor <ataylor@blitzagency.com>
2013-10-01 08:41:44 -0700

    Minor cleanup to tests.

--- tests/spec/blitz-selection-single.js
+++ tests/spec/blitz-selection-single.js
@@ -1,165 +1,172 @@
 define(function(require, exports, module) {
-    var selections = require('auf/ui/managers/selection');
 
-    describe('Single Selection Manager', function() {
+var selections = require('auf/ui/managers/selection');
 
-        var $items, manager = null;
+describe('Single Selection Manager', function() {
 
-        beforeEach(function() {
-            loadFixtures('selection-manager-single.html');
-            $items = $('.option');
-        });
+    var $items, manager = null;
 
-        afterEach(function() {
-            if(manager){
-                manager.close();
-                manager = null;
-            }
-        });
+    // Setup
+    // ==================================================================== //
 
-        it('should trigger before:select', function() {
-            manager = new selections.SingleSelectionManager({
-                el: $items
-            });
-
-            var beforeSelect = jasmine.createSpy('beforeSelect');
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect(beforeSelect).not.toHaveBeenCalled();
-
-            manager.listenTo(manager, 'before:select', beforeSelect);
-            $target.trigger('click');
-
-            expect(beforeSelect).toHaveBeenCalled();
-        });
-
-        it('should trigger after:select', function() {
-            manager = new selections.SingleSelectionManager({
-                el: $items
-            });
-
-            var afterSelect = jasmine.createSpy('afterSelect');
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect(afterSelect).not.toHaveBeenCalled();
-
-            manager.listenTo(manager, 'after:select', afterSelect);
-            $target.trigger('click');
-
-            expect(afterSelect).toHaveBeenCalled();
-        });
-
-        it('should select one option with click', function() {
-            manager = new selections.SingleSelectionManager({
-                el: $items
-            });
-
-            var $target1 = $items.eq(0);
-            var $target2 = $items.eq(1);
-            var selectedClass = manager.selectionManager.selectedClass;
-
-            $target1.trigger('click');
-            $target2.trigger('click');
-
-            expect($target1).not.toHaveClass(selectedClass);
-            expect($target2).toHaveClass(selectedClass);
-        });
-
-        it('should select one option on click with alt selectedClass', function() {
-            manager = new selections.SingleSelectionManager({
-                el: $items,
-                selectedClass: 'foo'
-            });
-
-            var $target1 = $items.eq(0);
-            var $target2 = $items.eq(1);
-
-            var selectedClass = 'foo';
-
-            $target1.trigger('click');
-            $target2.trigger('click');
-
-            expect($target1).not.toHaveClass(selectedClass);
-            expect($target2).toHaveClass(selectedClass);
-        });
-
-
-        it('should select one by index', function() {
-            manager = new selections.SingleSelectionManager({
-                el: $items
-            });
-
-            var $target1 = $items.eq(0);
-            var $target2 = $items.eq(1);
-            var selectedClass = manager.selectionManager.selectedClass;
-
-            manager.selectIndex(0);
-            manager.selectIndex(1);
-
-            expect($target1).not.toHaveClass(selectedClass);
-            expect($target2).toHaveClass(selectedClass);
-        });
-
-        it('should select one by value', function() {
-            manager = new selections.SingleSelectionManager({
-                el: $items
-            });
-
-            var $target1 = $items.eq(0);
-            var $target2 = $items.eq(1);
-            var selectedClass = manager.selectionManager.selectedClass;
-
-            manager.selectValue('one');
-            manager.selectValue('two');
-
-            expect($target1).not.toHaveClass(selectedClass);
-            expect($target2).toHaveClass(selectedClass);
-        });
-
-        it('should allow deselect', function() {
-            manager = new selections.SingleSelectionManager({
-                el: $items,
-                allowsDeselect: true
-            });
-
-            var $target1 = $items.eq(0);
-            var selectedClass = manager.selectionManager.selectedClass;
-
-            $target1.trigger('click');
-            expect($target1).toHaveClass(selectedClass);
-
-            $target1.trigger('click');
-            expect($target1).not.toHaveClass(selectedClass);
-        });
-
-        it('should return selected value', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            manager.selectIndex(1);
-            expect(manager.val()).toEqual('two');
-        });
-
-        it('should remove events', function() {
-            // we want to test for onClose on this manager
-            // specifically so don't use the module accessible
-            // 'manager' var, as it is closed for us in 'afterEach'
-
-            var scopedManager = new selections.SingleSelectionManager({
-                el: $items
-            });
-
-            scopedManager.close();
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect($target).not.toHaveClass(scopedManager.selectedClass);
-        });
+    beforeEach(function() {
+        loadFixtures('selection-manager-single.html');
+        $items = $('.option');
     });
-});
+
+    afterEach(function() {
+        if(manager){
+            manager.close();
+            manager = null;
+        }
+    });
+
+    // Test Suite
+    // ==================================================================== //
+
+    it('should trigger before:select', function() {
+        manager = new selections.SingleSelectionManager({
+            el: $items
+        });
+
+        var beforeSelect = jasmine.createSpy('beforeSelect'),
+            $target      = $items.eq(0);
+
+        $target.trigger('click');
+
+        expect(beforeSelect).not.toHaveBeenCalled();
+
+        manager.listenTo(manager, 'before:select', beforeSelect);
+        $target.trigger('click');
+
+        expect(beforeSelect).toHaveBeenCalled();
+    });
+
+    it('should trigger after:select', function() {
+        manager = new selections.SingleSelectionManager({
+            el: $items
+        });
+
+        var afterSelect = jasmine.createSpy('afterSelect'),
+            $target = $items.eq(0);
+
+        $target.trigger('click');
+
+        expect(afterSelect).not.toHaveBeenCalled();
+
+        manager.listenTo(manager, 'after:select', afterSelect);
+        $target.trigger('click');
+
+        expect(afterSelect).toHaveBeenCalled();
+    });
+
+    it('should select one option with click', function() {
+        manager = new selections.SingleSelectionManager({
+            el: $items
+        });
+
+        var $target1      = $items.eq(0),
+            $target2      = $items.eq(1),
+            selectedClass = manager.selectionManager.selectedClass;
+
+        $target1.trigger('click');
+        $target2.trigger('click');
+
+        expect($target1).not.toHaveClass(selectedClass);
+        expect($target2).toHaveClass(selectedClass);
+    });
+
+    it('should select one option on click with alt selectedClass', function() {
+        manager = new selections.SingleSelectionManager({
+            el: $items,
+            selectedClass: 'foo'
+        });
+
+        var $target1      = $items.eq(0),
+            $target2      = $items.eq(1),
+            selectedClass = 'foo';
+
+        $target1.trigger('click');
+        $target2.trigger('click');
+
+        expect($target1).not.toHaveClass(selectedClass);
+        expect($target2).toHaveClass(selectedClass);
+    });
+
+
+    it('should select one by index', function() {
+        manager = new selections.SingleSelectionManager({
+            el: $items
+        });
+
+        var $target1      = $items.eq(0),
+            $target2      = $items.eq(1),
+            selectedClass = manager.selectionManager.selectedClass;
+
+        manager.selectIndex(0);
+        manager.selectIndex(1);
+
+        expect($target1).not.toHaveClass(selectedClass);
+        expect($target2).toHaveClass(selectedClass);
+    });
+
+    it('should select one by value', function() {
+        manager = new selections.SingleSelectionManager({
+            el: $items
+        });
+
+        var $target1      = $items.eq(0),
+            $target2      = $items.eq(1),
+            selectedClass = manager.selectionManager.selectedClass;
+
+        manager.selectValue('one');
+        manager.selectValue('two');
+
+        expect($target1).not.toHaveClass(selectedClass);
+        expect($target2).toHaveClass(selectedClass);
+    });
+
+    it('should allow deselect', function() {
+        manager = new selections.SingleSelectionManager({
+            el: $items,
+            allowsDeselect: true
+        });
+
+        var $target1      = $items.eq(0),
+            selectedClass = manager.selectionManager.selectedClass;
+
+        $target1.trigger('click');
+        expect($target1).toHaveClass(selectedClass);
+
+        $target1.trigger('click');
+        expect($target1).not.toHaveClass(selectedClass);
+    });
+
+    it('should return selected value', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        manager.selectIndex(1);
+        expect(manager.val()).toEqual('two');
+    });
+
+    it('should remove events', function() {
+        // we want to test for onClose on this manager
+        // specifically so don't use the module accessible
+        // 'manager' var, as it is closed for us in 'afterEach'
+
+        var scopedManager = new selections.SingleSelectionManager({
+                el: $items
+            }),
+            $target = $items.eq(0);
+
+        scopedManager.close();
+
+        $target.trigger('click');
+
+        expect($target).not.toHaveClass(scopedManager.selectedClass);
+    });
+
+}); // eof describe
+}); // eof define
--- tests/spec/blitz-selection.js
+++ tests/spec/blitz-selection.js
@@ -1,212 +1,222 @@
 define(function(require, exports, module) {
-    var selections = require('auf/ui/managers/selection');
+var selections = require('auf/ui/managers/selection');
 
-    describe('Selection Manager', function() {
+describe('Selection Manager', function() {
 
-        var $items, manager = null;
+    var $items, manager = null;
 
-        beforeEach(function() {
-            loadFixtures('selection-manager.html');
-            $items = $('.option');
-        });
+    // Setup
+    // ==================================================================== //
 
-        afterEach(function() {
-            if(manager){
-                manager.close();
-                manager = null;
-            }
-        });
-
-        it('should trigger before:select', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            var beforeSelect = jasmine.createSpy('beforeSelect');
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect(beforeSelect).not.toHaveBeenCalled();
-
-            manager.listenTo(manager, 'before:select', beforeSelect);
-            $target.trigger('click');
-
-            expect(beforeSelect).toHaveBeenCalled();
-        });
-
-        it('should trigger after:select', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            var afterSelect = jasmine.createSpy('afterSelect');
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect(afterSelect).not.toHaveBeenCalled();
-
-            manager.listenTo(manager, 'after:select', afterSelect);
-            $target.trigger('click');
-
-            expect(afterSelect).toHaveBeenCalled();
-        });
-
-        it('should select 1st option with click', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            spyOn(manager, 'selectElement').andCallThrough();
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect($target).toHaveClass(manager.selectedClass);
-            expect(manager.selectElement).toHaveBeenCalled();
-        });
-
-        it('should select option with value', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-
-            var $target = $items.eq(1);
-            manager.selectValue('two');
-            expect($target).toHaveClass(manager.selectedClass);
-        });
-
-        it('should return selected value', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            manager.selectIndex(1);
-            expect(manager.val()).toEqual('two');
-        });
-
-        it('should return null for selected value', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            expect(manager.val()).toEqual(null);
-        });
-
-        it('should deselect', function() {
-            manager = new selections.SelectionManager({
-                el: $items,
-                allowsDeselect: true
-            });
-
-            var $target = $items.eq(1);
-            manager.selectIndex(1);
-
-            expect($target).toHaveClass(manager.selectedClass);
-
-            manager.selectIndex(1);
-            expect($target).not.toHaveClass(manager.selectedClass);
-        });
-
-        it('should not deselect', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            var $target = $items.eq(1);
-
-            manager.selectIndex(1);
-            expect($target).toHaveClass(manager.selectedClass);
-
-            manager.selectIndex(1);
-            expect($target).toHaveClass(manager.selectedClass);
-        });
-
-        it('should select all options with click', function() {
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            $items.trigger('click');
-            expect($items.eq(0)).toHaveClass(manager.selectedClass);
-            expect($items.eq(1)).toHaveClass(manager.selectedClass);
-            expect($items.eq(2)).toHaveClass(manager.selectedClass);
-        });
-
-        it('should set alternate selectedClass', function() {
-            manager = new selections.SelectionManager({
-                el: $items,
-                selectedClass: 'foo'
-            });
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect(manager.selectedClass).toBe('foo');
-            expect($target).toHaveClass(manager.selectedClass);
-        });
-
-        it('should set call selection delegate', function() {
-            var delegate = jasmine.createSpyObj('delegate', ['selectionManagerShouldSelect']);
-            manager = new selections.SelectionManager({
-                el: $items,
-                delegate: delegate
-            });
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect(delegate.selectionManagerShouldSelect).toHaveBeenCalled();
-            expect(delegate.selectionManagerShouldSelect).toHaveBeenCalledWith($target);
-        });
-
-        it('should select index 1', function() {
-
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            manager.selectIndex(1);
-
-            var $target = $items.eq(1);
-            expect($target).toHaveClass(manager.selectedClass);
-        });
-
-        it('should select all indexes', function() {
-
-            manager = new selections.SelectionManager({
-                el: $items
-            });
-
-            manager.selectIndex(0);
-            manager.selectIndex(1);
-            manager.selectIndex(2);
-
-            expect($items.eq(0)).toHaveClass(manager.selectedClass);
-            expect($items.eq(1)).toHaveClass(manager.selectedClass);
-            expect($items.eq(2)).toHaveClass(manager.selectedClass);
-        });
-
-        it('should remove events', function() {
-            // we want to test for onClose on this manager
-            // specifically so don't use the module accessible
-            // 'manager' var, as it is closed for us in 'afterEach'
-
-            var scopedManager = new selections.SelectionManager({
-                el: $items
-            });
-
-            spyOn(scopedManager, 'selectElement').andCallThrough();
-            scopedManager.close();
-
-            var $target = $items.eq(0);
-            $target.trigger('click');
-
-            expect(scopedManager.selectElement).not.toHaveBeenCalled();
-            expect($target).not.toHaveClass(scopedManager.selectedClass);
-        });
-
+    beforeEach(function() {
+        loadFixtures('selection-manager.html');
+        $items = $('.option');
     });
-});
+
+    afterEach(function() {
+        if(manager){
+            manager.close();
+            manager = null;
+        }
+    });
+
+    // Test Suite
+    // ==================================================================== //
+
+    it('should trigger before:select', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        var beforeSelect = jasmine.createSpy('beforeSelect'),
+            $target      = $items.eq(0);
+
+        $target.trigger('click');
+
+        expect(beforeSelect).not.toHaveBeenCalled();
+
+        manager.listenTo(manager, 'before:select', beforeSelect);
+        $target.trigger('click');
+
+        expect(beforeSelect).toHaveBeenCalled();
+    });
+
+    it('should trigger after:select', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        var afterSelect = jasmine.createSpy('afterSelect'),
+            $target     = $items.eq(0);
+
+        $target.trigger('click');
+
+        expect(afterSelect).not.toHaveBeenCalled();
+
+        manager.listenTo(manager, 'after:select', afterSelect);
+        $target.trigger('click');
+
+        expect(afterSelect).toHaveBeenCalled();
+    });
+
+    it('should select 1st option with click', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        spyOn(manager, 'selectElement').andCallThrough();
+
+        var $target = $items.eq(0);
+
+        $target.trigger('click');
+
+        expect($target).toHaveClass(manager.selectedClass);
+        expect(manager.selectElement).toHaveBeenCalled();
+    });
+
+    it('should select option with value', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        var $target = $items.eq(1);
+
+        manager.selectValue('two');
+        expect($target).toHaveClass(manager.selectedClass);
+    });
+
+    it('should return selected value', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        manager.selectIndex(1);
+        expect(manager.val()).toEqual('two');
+    });
+
+    it('should return null for selected value', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        expect(manager.val()).toEqual(null);
+    });
+
+    it('should deselect', function() {
+        manager = new selections.SelectionManager({
+            el: $items,
+            allowsDeselect: true
+        });
+
+        var $target = $items.eq(1);
+
+        manager.selectIndex(1);
+
+        expect($target).toHaveClass(manager.selectedClass);
+
+        manager.selectIndex(1);
+        expect($target).not.toHaveClass(manager.selectedClass);
+    });
+
+    it('should not deselect', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        var $target = $items.eq(1);
+
+        manager.selectIndex(1);
+        expect($target).toHaveClass(manager.selectedClass);
+
+        manager.selectIndex(1);
+        expect($target).toHaveClass(manager.selectedClass);
+    });
+
+    it('should select all options with click', function() {
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        $items.trigger('click');
+        expect($items.eq(0)).toHaveClass(manager.selectedClass);
+        expect($items.eq(1)).toHaveClass(manager.selectedClass);
+        expect($items.eq(2)).toHaveClass(manager.selectedClass);
+    });
+
+    it('should set alternate selectedClass', function() {
+        manager = new selections.SelectionManager({
+            el: $items,
+            selectedClass: 'foo'
+        });
+
+        var $target = $items.eq(0);
+        $target.trigger('click');
+
+        expect(manager.selectedClass).toBe('foo');
+        expect($target).toHaveClass(manager.selectedClass);
+    });
+
+    it('should set call selection delegate', function() {
+        var delegate = jasmine.createSpyObj('delegate', ['selectionManagerShouldSelect']);
+        manager = new selections.SelectionManager({
+            el: $items,
+            delegate: delegate
+        });
+
+        var $target = $items.eq(0);
+
+        $target.trigger('click');
+
+        expect(delegate.selectionManagerShouldSelect).toHaveBeenCalled();
+        expect(delegate.selectionManagerShouldSelect).toHaveBeenCalledWith($target);
+    });
+
+    it('should select index 1', function() {
+
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        manager.selectIndex(1);
+
+        var $target = $items.eq(1);
+
+        expect($target).toHaveClass(manager.selectedClass);
+    });
+
+    it('should select all indexes', function() {
+
+        manager = new selections.SelectionManager({
+            el: $items
+        });
+
+        manager.selectIndex(0);
+        manager.selectIndex(1);
+        manager.selectIndex(2);
+
+        expect($items.eq(0)).toHaveClass(manager.selectedClass);
+        expect($items.eq(1)).toHaveClass(manager.selectedClass);
+        expect($items.eq(2)).toHaveClass(manager.selectedClass);
+    });
+
+    it('should remove events', function() {
+        // we want to test for onClose on this manager
+        // specifically so don't use the module accessible
+        // 'manager' var, as it is closed for us in 'afterEach'
+
+        var scopedManager = new selections.SelectionManager({
+                el: $items
+            }),
+            $target = $items.eq(0);
+
+        spyOn(scopedManager, 'selectElement').andCallThrough();
+        scopedManager.close();
+
+        $target.trigger('click');
+
+        expect(scopedManager.selectElement).not.toHaveBeenCalled();
+        expect($target).not.toHaveClass(scopedManager.selectedClass);
+    });
+
+}); // eof desribe
+}); // eof define
