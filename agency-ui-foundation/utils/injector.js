77a768d3b070da48baba2177cd29ccb1ba4fea5f
Adam Venturella <aventurella@gmail.com>
2013-10-02 11:43:15 -0700

    created utils folder added injector

---
+++ agency-ui-foundation/utils/injector.js
@@ -0,0 +1,62 @@
+define(function(require){
+
+    var Marionette = require('marionette'),
+        _ = require('underscore'),
+        logging = require('alf/logging'),
+        log = logging.getLogger('alf.injector');
+
+    var Component = Marionette.Controller.extend({
+        cls: null,
+        kwargs:null,
+        key:null,
+        container: null,
+        dependencies: null,
+
+        initialize: function(options){
+            this.dependencies = [];
+            _.extend(this, options);
+        },
+
+        depends: function(key, cls, kwargs){
+            this.dependencies.push(new Component({cls:cls, kwargs:kwargs, key:key}));
+            return this;
+        },
+
+        call: function(kwargs){
+            var args = {};
+            kwargs = kwargs || {};
+
+            _.map(this.dependencies, function(x){
+                var data = {};
+                var obj = x.call(x.kwargs);
+                data[x.key] = obj;
+                _.extend(args, data);
+            });
+
+            target = this.cls;
+            obj = new target(_.extend(args, kwargs));
+            return obj;
+        }
+    });
+
+    var Container = Marionette.Controller.extend({
+        registry: {},
+
+        register: function(name, cls, kwargs){
+            var component = new Component({cls:cls});
+            component.container = this;
+            log.debug('Registering ' + name);
+            this.registry[name.toLowerCase()] = component;
+            return component;
+        },
+
+        resolve: function(name){
+            name = name.toLowerCase();
+            var component = this.registry[name];
+            return component.call();
+        }
+    });
+
+    return Container;
+});
+
