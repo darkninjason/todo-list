c63a46083c7549e335930111c253d4e1dd3dd920
Aubrey Taylor <ataylor@blitzagency.com>
2013-10-09 14:34:58 -0700

    Removed slider tests from suite (termporary).

--- tests/spec/index.js
+++ tests/spec/index.js
@@ -13,8 +13,8 @@ define(function() {
       'spec/auf-managers-range',
       'spec/auf-managers-focus',
       'spec/auf-managers-focus-single',
-      'spec/auf-control-slider-horizontal',
-      'spec/auf-control-slider-horizontal-range',
+      // 'spec/auf-control-slider-horizontal',
+      // 'spec/auf-control-slider-horizontal-range',
     ]
   };
 });
