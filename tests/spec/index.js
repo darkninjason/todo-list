6c67f9c49559af67b24427ab8d20844ecd62f175
Adam Venturella <aventurella@gmail.com>
2013-10-14 12:05:33 -0700

    updated html running with new tests

--- tests/spec/index.js
+++ tests/spec/index.js
@@ -1,21 +1,23 @@
 define(function() {
   return {
     specs: [
-      'spec/auf-jquery-collection',
-      'spec/auf-calendar',
-      'spec/auf-datetime',
-      'spec/auf-datetime-formats',
-      'spec/auf-responders-keys',
-      'spec/auf-responders-touches',
-      'spec/auf-responders-mouse',
-      'spec/auf-gestures-swipe',
-      'spec/auf-managers-index',
-      'spec/auf-managers-range',
-      'spec/auf-managers-focus',
-      'spec/auf-managers-focus-single',
-      'spec/auf-control-slider-horizontal',
-      'spec/auf-control-slider-horizontal-range',
-      'spec/auf-control-input-select'
+     'spec/auf-utils-helpers',
+     'spec/auf-jquery-collection',
+     'spec/auf-calendar',
+     'spec/auf-datetime',
+     'spec/auf-datetime-formats',
+     'spec/auf-responders-keys',
+     'spec/auf-responders-touches',
+     'spec/auf-responders-mouse',
+     'spec/auf-responders-orientation',
+     'spec/auf-gestures-swipe',
+     'spec/auf-managers-index',
+     'spec/auf-managers-range',
+     'spec/auf-managers-focus',
+     'spec/auf-managers-focus-single',
+     'spec/auf-control-slider-horizontal',
+     'spec/auf-control-slider-horizontal-range',
+    'spec/auf-control-input-select'
     ]
   };
 });
