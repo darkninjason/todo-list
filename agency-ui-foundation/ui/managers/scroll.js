6bbf5d8b141a1f53074b8794e21c468d3a837bab
Aubrey Taylor <aubricus@gmail.com>
2013-10-17 08:50:51 -0700

    Update, added some implmentation
    - Stubbed out a lot of the methods and added very untested
      implementation.

--- agency-ui-foundation/ui/managers/scroll.js
+++ agency-ui-foundation/ui/managers/scroll.js
@@ -1,71 +1,27 @@
 define(function(require, exports, module){
 
 // Imports
+var Backbone   = require('backbone');
 var Marionette = require('marionette');
 var _          = require('underscore');
-var helpers    = require('auf/utils/helpers');
+
+var Helpers         = require('auf/utils/helpers');
+var ScrollResponder = require('auf/ui/responders/scroll');
+var RangeManager    = require('auf/ui/managers/range');
 
 // Module
-
-// TODO:
-// Public API:
-// addMarkerValue(pixel-value, whole-number)
-// addMarkerPosition(percent)
-// removeMarkerValue(pixel-value, whole-number)
-// removeMarkerPosition(percent)
-// addMarkersUsingElements($('.selector'))
-// removeMarkersUsingElements($('.selector'))
-// setScrollPosition()
-// setScrollValue()
-// getMarkers() // return array [{position, value}]
-//
-// These might be stupid
-// removeMarker(index || object-ref)
-//
-// Thoughts:
-//
-// Horizontal or Vertical?
-//
-// Call this VerticalScrollManager to match slider names?
-//
-// How to store markers?
-//
-// Waypoints supports element offsets which I think is useful
-// - could change elements sig to use {el:$('selector') or 'string', offset:-10}
-// ScrollTop can be a pain in the ass
-// - We may want to create a 'special' element called 'top' (or perhaps a flag, useTop) which will
-//   cause this component to go find the appropriate "scrollable" element.
-// - Check out vizio for x-browser compaitble, jqurey based test for scrollTop.
-//
-// Flow:
-// Like slider, range manager "change" should drive all updates.
-// This should centralize code and make it easier to debug code paths
-//
-// - receives "scroll" event from scroll responder
-//   - looks up scrollTop (or equiv) from registered element
-//   - querys range manager for value for position
-//   - calls _updateRangePosition to update range manager
-//   - call to range manager causes change event
-//   - check markers, dispatch marker event if one exists
-//
-// - user sets scroll position or value
-//   - querys range manager for value for position, if value
-//   - calls _updateRangePosition to update range manager
-//   - call to RM caues change event
-//   - do we check to see if we've passed any markers when setting scroll?
-//   - if so, check to see which markers we've passed, fire events for all
-//   - is scroll animated?
 
 var ScrollManager = Marionette.Controller.extend({
 
     EVENT_MARKER: 'marker',
 
     scrollResponder: null,
-    rangeManager: null,
-    markers: [], // TODO: This will probably change
+    rangeManager   : null,
+    markers        : [],
 
     _defaults: {
         el: null,
+        animates: false
     },
 
     // Backbone & Marionette overrides
@@ -77,11 +33,10 @@ var ScrollManager = Marionette.Controller.extend({
             throw 'No input element provided.';
         }
 
-        this.$el = helpers.getElement(this.options.el);
+        this.$el = Helpers.getElement(this.options.el);
 
         this.scrollResponder = this._initializeScrollResponder(this.options);
         this.rangeManager    = this._initializeRangeManager(this.options);
-
     },
 
     onClose: function() {
@@ -91,122 +46,185 @@ var ScrollManager = Marionette.Controller.extend({
     // Initialization
 
     _initializeScrollResponder: function(options) {
-        // init scroll responder
-        return null;
+        return new ScrollResponder({
+            el: options.el,
+            scroll: _bind(this._didReceiveScroll, this)
+        });
     },
 
     _didReceiveScroll: function(responder, e) {
-        // implement this
-        // should set position in range manager?
+        console.log('_didReceiveScroll not implemented');
+        return;
+
+        var scroll, position;
+
+        scroll = this._getElementScroll(this.$el);
+
+        // TODO: Revisit - Horizontal will need update
+        position = this.rangeManager.getPositionForValue(scroll.x);
+        this._updateRangePosition(position);
     },
 
     _initializeRangeManager: function(options) {
-        // init range manager
-        return null;
+        var manager, max;
+
+        max     = this._getElementBounds(this.$el);
+        manager = new RangeManager({
+            min: 0,
+            max: max
+        });
+
+        this.listenTo(manager, _.bind(this._rangeManagerDidChange, this));
+
+        return manager;
+    },
+
+    _getElementBounds: function($el) {
+        // returns ClientRect: {'bottom', 'height', 'left', 'right', 'top', 'width'}
+        // TODO: Simlar Code - see horzontal.js "_getElementBounds"
+        // - May need to move this into utils?
+        return $el[0].getBoundingClientRect();
     },
 
     _rangeManagerDidChange: function(sender, position, value) {
-        // implement this
+        console.log('_rangeManagerDidChange not implemented', position, value);
     },
 
-    // This seems redundant, but a few functions will update the range
-    // thought this might centralize calls.
+    // All updates to range manager position should route through here.
     _updateRangePosition: function(position) {
-        // this.rangeManager.setPosition(position);
+        this.rangeManager.setPosition(position);
     },
 
     // "Private" internal methods
 
-    _addMarker: function(position) {
-        // push marker into marker array
+    _sortArrayAscending: function(arr) {
+        // see: http://bit.ly/1c0cPTU
+        arr.sort(function(a, b){
+            return a - b;
+        });
     },
 
-    _addMarkerAt: function(index, position) {
-        // insert marker into specificed index
-        // arr = []
-        // arr[2] = '0.8'
-        // returns
-        // [undefined, undefined, 0.8]
+    _getElementScroll: function($el) {
+        var isWindow, el;
+
+        el = $el[0];
+        isWindow = window === _.identity(el);
+
+        if(isWindow) {
+            // TODO: Revisit - x-browser, ie8 does not support page[X|Y]Offset.
+            // see: http://mzl.la/19SZOty
+            return {
+                x: el.pageXOffset,
+                y: el.pageYOffset
+            };
+
+        }else{
+            return {
+                x: el.scrollLeft,
+                y: el.scrollTop
+            };
+        }
     },
 
-    _removeMarkerAt: function(index) {
-        // remove marker from marker array
-    },
+    _setElementScroll: function($el, x, y) {
+        var isWindow, el;
 
-    _removeMarkerObjectRef: function(ref) {
-        // index of object ref
-        // call removeMarkerAt(index)
+        isWindow = window === _.identity($el[0]);
+
+        if(isWindow) {
+            // TODO: Revisit - x-browser compatiblity. (probably ie-hate)
+            $el[0].scrollTo(x, y);
+        }else{
+            el = $el[0];
+
+            el.scrollLeft = x;
+            el.scrollTop  = y;
+        }
     },
 
     // Public API
 
     getMarkers: function() {
-        // readonly
-        // return whatever storage mechanism holding markers
-        return null;
+        // return (shallow) copy of markers
+        return this.markers.slice();
     },
 
-    addMarkerPosition: function(position) {
-        // call _addMarker(position)
+    addMarker: function(position) {
+        var markerExists;
+
+        markerExists = _.indexOf(this.markers, position, true) != -1;
+
+        if(markerExists) {
+            return false;
+        }
+
+        markers.push(position);
+        markers.sort(this._sortArrayAscending);
+
+        return true;
     },
 
-    removeMarkerPosition: function(position) {
-        // index of position
-        // call _removeMarkerAt(index)
+    removeMarker: function(position) {
+        var markerIndex, markerDoesNotExist;
 
-        // TODO: potential here for more than one marker
-        // to exist in list, need to revisit.
+        markerIndex        = _.indexOf(this.markers, position);
+        markerDoesNotExist = markerIndex == -1;
+
+        if(markerDoesNotExist) {
+            return false;
+        }
+
+        this.markers.splice(markerIndex, 1);
+        this.markers.sort(this._sortArrayAscending);
+
+        return true;
     },
 
     addMarkerValue: function(value) {
-        // get marker position equivelant
-        // call _addMarker
+        position = this.rangeManager.getPositionForValue(value);
+        this.addMarker(position);
     },
 
     removeMarkerValue: function(value) {
-        // get position for value
-        // index of position
-        // call _removeMarkerAt(index)
+        position = this.rangeManager.getPositionForValue(value);
+        this.removeMarker(position);
     },
 
     addMarkersUsingElements: function($els) {
-        // each, get top for each element
-        // get position for each top or left (for horizontal)
-        // call _addMarker(position)
-        //
-        // TODO:
-        // Do we track what elements have been added to reduce duplicate events?
-        // An array may not be sufficient here.
+        console.log('addMarkersUsingElements not implemented', $els);
     },
 
     removeMarkersUsingElements: function($els) {
-        // each, get top for each element
-        // get position for each top
-        // call _removeMarker(position)
-        //
-        // TODO: I'm thinking an array isn't going to work here
-        // if you pass in an element with the same position as
-        // something you previous specified in addMarkerPosition
-        // you may inadvertently remove that position.
-        // Need to think this through later.
+        console.log('removeMarkersUsingElements not implemented', $els);
     },
 
     setScrollPosition: function(position) {
-        // TODO: need good browser scrollTop if using window, html, or body.
-        // set value in range manager
-        // scroll top should be updated in range manager did change
+        var value;
+
+        value = this.rangeManager.getValueForPosition(position);
+        setScrollValue(value);
+    },
+
+    getScrollPosition: function() {
+        return this.rangeManager.position;
     },
 
     setScrollValue: function(value) {
-        // get position for value
-        // call setScrollPosition
+        // TODO: Revisit - will need update for horizontal scroll
+        this._setElementScroll(this.$el, 0, value);
+    },
+
+    getScrollValue: function() {
+        return this.rangeManager.value;
     },
 
     // Event dispatchers
 
     _dispatchMarker: function() {
-        var position = this.rangeManager.getPosition();
-        var value    = this.rangeManager.getValue();
+        var position, value;
+
+        position = this.rangeManager.getPosition();
+        value    = this.rangeManager.getValue();
 
         this.trigger(this.EVENT_MARKER, this, position, value);
     },
