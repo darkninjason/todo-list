04755021673c67d4cdb6cb06f0d43290bb641492
Aubrey Taylor <aubricus@gmail.com>
2013-10-19 21:51:51 -0700

    Update, fleshed out most of scroll.js
    - Add and Remove methods now take args
    - Add and Remove using element methods are working
    - Removed non-primitive initialization from top of object literal
    - Minor improvements

--- agency-ui-foundation/ui/managers/scroll.js
+++ agency-ui-foundation/ui/managers/scroll.js
@@ -13,37 +13,26 @@ var RangeManager    = require('auf/ui/managers/range');
 
 // TODO:
 // - WindowResponder, resize event implementation. Compose here to get resize.
-
-// - implement calculateMaxScroll
-// - implement add markers for elements
-
-// - setPosition|Value -> setPositions|Values that takes *args
-
 // - Move markers funct to range or marked ranges
-
 // - SmoothScroll, composes scroll manager
-
 // - Forward a 'scroll' event that sends range position / value
-
-// - Use _.uniq to filter out duplicate markers
 
 var ScrollManager = Marionette.Controller.extend({
 
     EVENT_MARKER: 'marker',
-    EVENT_CHANGE: 'change',
+    EVENT_SCROLL: 'scroll',
+
+    _defaults: null,
 
     scrollResponder: null,
     rangeManager   : null,
     scrollable     : null,
 
-    markers          : [],
-    prevPosition     : null,
-
-    _defaults: {
-        el     : null,
-        smooth : false,
-        markers: null
-    },
+    // marker values should be unique
+    // sort markers after adding a value
+    markers       : null,
+    lastDispatched: null,
+    prevPosition  : null,
 
     // Backbone & Marionette overrides
 
@@ -60,15 +49,30 @@ var ScrollManager = Marionette.Controller.extend({
      * );
      */
     initialize: function(options) {
+        this._defaults = {
+            el: null
+        };
+
+        // apply defaults to options
         _.defaults(options, this._defaults);
 
         if(_.isEmpty(this.options.el)) {
             throw 'No input element provided.';
         }
 
+        this.markers         = [];
+        this.lastDispatched  = [];
         this.$el             = Helpers.getElement(this.options.el);
         this.scrollResponder = this._initializeScrollResponder(this.options);
         this.rangeManager    = this._initializeRangeManager(this.options);
+
+        // call this for initial max scroll setting
+        this.calculateMaxScroll();
+
+        // listen to range manager
+        this.listenTo(
+            this.rangeManager, 'change', _.bind(this._rangeManagerDidChange, this)
+        );
     },
 
     onClose: function() {
@@ -86,12 +90,12 @@ var ScrollManager = Marionette.Controller.extend({
     },
 
     _didReceiveScroll: function(responder, e) {
-        var scroll, position;
+        var scrollable, position;
 
-        scroll = this._getElementScroll(this.$el);
-
-        // TODO: Revisit - Horizontal will need update
-        position = this.rangeManager.calculatePositionForValue(scroll.y);
+        scrollable = this.getScrollable(this.$el);
+        position   = this.rangeManager.calculatePositionForValue(
+            scrollable.scrollTop
+        );
 
         this._updateRangePosition(position);
     },
@@ -99,18 +103,7 @@ var ScrollManager = Marionette.Controller.extend({
     _initializeRangeManager: function(options) {
         var manager, max, listener, scrollable, start;
 
-        // TODO: Revisit - Horizontal will need upate.
-        scrollable = this.getScrollable(this.$el);
-        max        = scrollable.scrollHeight - scrollable.displayHeight;
-        listener   = _.bind(this._rangeManagerDidChange, this);
-
-        manager = new RangeManager({
-            min: 0,
-            max: max
-        });
-
-        this.listenTo(manager, 'change', listener);
-
+        manager = new RangeManager();
         return manager;
     },
 
@@ -144,8 +137,11 @@ var ScrollManager = Marionette.Controller.extend({
         // loop over all markers to see if we're in range
         _.each(this.markers, iterator, this);
 
+        reached = _.difference(reached, this.lastDispatched);
+
         if(reached.length > 0) {
             this._dispatchMarker(reached, direction);
+            this.lastDispatched = reached;
         }
 
         // finally update our local storage of position
@@ -168,13 +164,26 @@ var ScrollManager = Marionette.Controller.extend({
     // Public API
 
     getScrollable: function($el) {
-        var el, bounds;
+        var el, isWindow, documentElement, body, scrollTop, scrollLeft;
 
-        // If the window was provided, use the
-        // documenetElement, usually 'html', for dimensions.
-        // TODO: Revisit - Not sure about mobile compat.
-        $el = (this._elementIsWindow($el)) ? $(document.documentElement) : $el;
-        el  = $el[0];
+        isWindow        = this._elementIsWindow($el);
+        documentElement = document.documentElement;
+        body            = document.body;
+
+        $el        = (isWindow) ? $(documentElement) : $el;
+        el         = $el[0];
+
+        // see: http://mzl.la/19SZOty
+        // for detailed explanation of ternary test below
+        //
+        // essentially:
+        // documentElement will sometimes return zero for scroll top,
+        // even when the window is scrolled. If it does return zero,
+        // check the body for it's scroll top. This technique should
+        // always return a value equal to the what window's pageYOffset
+        // would have been should pageYOffset be unsupported.
+        scrollTop  = (isWindow) ? (window.pageYOffset || documentElement.scrollTop  || body.scrollTop ) : el.scrollTop;
+        scrollLeft = (isWindow) ? (window.pageXOffset || documentElement.scrollLeft || body.scrollLeft) : el.scrollLeft;
 
         // see: http://mzl.la/19VEUIo
         // for a guide to these properties
@@ -184,7 +193,9 @@ var ScrollManager = Marionette.Controller.extend({
             displayHeight: el.clientHeight,
             displayWidth : el.clientWidth,
             scrollHeight : el.scrollHeight,
-            scrollWidth  : el.scrollWidth
+            scrollWidth  : el.scrollWidth,
+            scrollTop    : scrollTop,
+            scrollLeft   : scrollLeft
         };
     },
 
@@ -193,66 +204,150 @@ var ScrollManager = Marionette.Controller.extend({
         return this.markers.slice();
     },
 
-    addMarkerPosition: function(position) {
-        var markerExists;
+    calculateMaxScroll: function() {
+        var scrollable, max;
 
-        markerExists = _.indexOf(this.markers, position, true) != -1;
+        scrollable = this.getScrollable(this.$el);
+        max = scrollable.scrollHeight - scrollable.displayHeight;
 
-        if(markerExists) {
-            return false;
+        this.rangeManager.setMax(max);
+    },
+
+    /**
+     * add positions (values between 0 and 1) to the markers array
+     * @param {arguments} __args__ position arguments
+     *
+     * @usage addMarkerPositions([*positions])
+     * @example addMerkerPositions(0.1, 0.2, 0.3)
+     */
+    addMarkerPositions: function(__args__) {
+        // __args__ is there to denote that this function takes 'arguments'
+        // if you're familiar with python it's inspired by *args.
+        // unfortunately, * is not a legal character for function args in js.
+        // I added it to make the signature more explicit
+        var sorted;
+
+        function iterator(position, i, list) {
+            if(position < 0 || position > 1) {
+                throw 'Position out of range. Valid positions are between 0 and 1, position: ' + position;
+            }
+
+            this.markers.push(position);
         }
 
-        this.markers.push(position);
-        this.markers.sort(this._sortArrayAscending);
+        sorted = true;
 
-        return true;
+        // add positions to this.markers
+        _.each(arguments, iterator, this);
+
+        this.markers.sort(this._sortArrayAscending);
+        this.markers = _.uniq(this.markers, sorted);
     },
 
-    removeMarkerPosition: function(position) {
-        var markerIndex, markerDoesNotExist;
+    removeMarkerPositions: function(__args__) {
+        var markerIndex;
 
-        markerIndex        = _.indexOf(this.markers, position);
-        markerDoesNotExist = markerIndex == -1;
+        function iterator(arg, i, list) {
+            markerIndex = _.indexOf(this.markers, arg);
 
-        if(markerDoesNotExist) {
-            return false;
+            if(markerIndex == -1) {
+                return;
+            }
+
+            this.markers.splice(markerIndex, 1);
         }
 
-        this.markers.splice(markerIndex, 1);
-        this.markers.sort(this._sortArrayAscending);
-
-        return true;
+        // attempt removal of requested positions
+        // non-existing positions are ignored
+        _.each(arguments, iterator, this);
     },
 
-    addMarkerValue: function(value) {
-        position = this.rangeManager.calculatePositionForValue(value);
-        return this.addMarkerPosition(position);
+    addMarkerValues: function(__args__) {
+        var positions;
+
+        positions = _.map(
+            arguments,
+            this.rangeManager.calculatePositionForValue,
+            this.rangeManager
+        );
+
+        this.addMarkerPositions.apply(this, positions);
     },
 
-    removeMarkerValue: function(value) {
-        position = this.rangeManager.calculatePositionForValue(value);
-        return this.removeMarkerPosition(position);
+    removeMarkerValues: function(__args__) {
+        var positions;
+
+        positions = _.map(
+            arguments,
+            this.rangeManager.calculatePositionForValue,
+            this.rangeManager
+        );
+
+        this.removeMarkerPositions.apply(this, positions);
     },
 
-    addMarkersUsingElements: function($els) {
-        // should return dict with resulting markers for keys.
-        console.log('addMarkersUsingElements not implemented', $els);
+    /**
+     * Add markers using the 'top' position of the elements
+     * @param {jquery selector} $elements the elements to add markers for
+     *
+     * @returns {object} a reference dictionary, {position: $element}
+     *
+     * @notes
+     * - This function WILL NOT add markers for element's who's top is
+     *   greater than the max scroll.
+     */
+    addMarkersUsingElements: function($elements) {
+        var $el, $position, dict, top, position, positions;
+
+        function iterator(el, i, list) {
+            $el = $(el);
+            $position = $el.position();
+            top = $position.top;
+
+            if(top < this.rangeManager.getMax()) {
+                position = this.rangeManager.calculatePositionForValue(top);
+
+                dict[position+''] = $el;
+
+                positions.push(position);
+            }
+        }
+
+        positions = [];
+        dict = {};
+
+        _.each($elements, iterator, this);
+
+        this.addMarkerPositions.apply(this, positions);
+
+        return dict;
     },
 
-    removeMarkersUsingElements: function($els) {
-        // return dict with success / fail?
-        console.log('removeMarkersUsingElements not implemented', $els);
+    removeMarkersUsingElements: function($elements) {
+        var $el, $position, top, position, positions;
+
+        function iterator(el, i, list) {
+            $el = $(el);
+            $position = $el.position();
+            top = $position.top;
+
+            return this.rangeManager.calculatePositionForValue(top);
+        }
+
+        positions = _.map($elements, iterator, this);
+
+        this.removeMarkerPositions.apply(this, positions);
     },
 
     // Event dispatchers
 
-    _dispatchChange: function() {
+    _dispatchScroll: function() {
         var position, value;
 
         position = this.rangeManager.getPosition();
         value    = this.rangeManager.getValue();
 
-        this.trigger(this.EVENT_CHANGE, this, position, value);
+        this.trigger(this.EVENT_SCROLL, this, position, value);
     },
 
     _dispatchMarker: function(markers, direction) {
