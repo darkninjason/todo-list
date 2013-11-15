432bb1273b0de1f392332c07a8f36095e9368c38
dinopetrone <dinopetrone@gmail.com>
2013-11-15 15:46:57 -0800

    abstracting some logic out of input select to fall in line with marionette usage

--- static/js/app/modules/demo-input-select/views/input-select.js
+++ static/js/app/modules/demo-input-select/views/input-select.js
@@ -1,7 +1,7 @@
 define(function (require, exports, module) {
 
 var marionette = require('marionette');
-var InputSelect = require('auf/ui/controls/forms/input-select').InputSelect;
+var InputSelect = require('auf/components/input-select').InputSelectMarionette;
 var template = require('hbs!tpl/input-select/composite');
 var ResultItem = require('./result-item').ResultItem;
 var InputResults = require('../collections').InputResults;
@@ -21,9 +21,6 @@ var InputSelectView = marionette.CompositeView.extend({
             el: this.ui.input
         });
         this.listenTo(this.inputSelect, 'input', this.onInputChange);
-        this.listenTo(this.inputSelect, 'blur', this.onItemBlur);
-        this.listenTo(this.inputSelect, 'focus', this.onItemFocus);
-        this.listenTo(this.inputSelect, 'select', this.onItemSelect);
         this.listenTo(this.collection,'sync',this.onCollectionSync);
     },
 
@@ -32,46 +29,9 @@ var InputSelectView = marionette.CompositeView.extend({
     },
 
     onCollectionSync: function(){
-        var elements = [];
-        var views = this.children.toArray();
-        _.each(views, function(each){
-            elements.push(each.$el[0]);
-        });
-        this.inputSelect.setElements($(elements));
-        var mDict = this.mDict = {};
-        _.each(views, function(each){
-            var key = each.$el.data('auf-id');
-            mDict[key] = each;
-            elements.push(each.$el[0]);
-        });
+        this.inputSelect.setViews(this.children);
         this.inputSelect.beginNavigationPhase();
-    },
-
-    getViewForElement: function($element){
-
-        return itemView;
-    },
-
-    triggerEventOnViewForElement: function(event, $element){
-        var key = $element.data('auf-id');
-        var itemView = this.mDict[key];
-        if(itemView){
-            itemView.trigger(event);
-        }
-    },
-
-    onItemBlur: function(input, $element){
-        this.triggerEventOnViewForElement('blur', $element);
-    },
-
-    onItemFocus: function(input, $element){
-        this.triggerEventOnViewForElement('focus', $element);
-    },
-
-    onItemSelect: function(input, $element){
-        this.triggerEventOnViewForElement('select', $element);
-    },
-
+    }
 });
 
 exports.InputSelectView = InputSelectView;
