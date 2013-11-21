ff713bffc7f20c8b6c0024fd9b68c55f09baed22
dinopetrone <dinopetrone@gmail.com>
2013-11-20 19:38:36 -0800

    adding objectFromElement and modelFromElement helpers

--- built/ui/helpers/dom.js
+++ built/ui/helpers/dom.js
@@ -3,11 +3,33 @@ define(function (require, exports, module) {
 var marionette = require('marionette');
 var getElement = require('built/core/utils/helpers').getElement;
 
+
+function objectFromElement($element, map){
+    var key;
+    map = map || {};
+    $element = getElement($element);
+    var node = $element[0];
+    var attrs = {};
+
+    for(var i=0; i < node.attributes.length; i++){
+        var obj = node.attributes[i];
+        key = map[obj.name] || obj.name;
+        attrs[key] = obj.value;
+    }
+    key = map['content'] || 'content';
+    attrs[key] = $element.text();
+    return attrs;
+}
+
 function modelFromElement($element, Model, map){
     Model = Model || Backbone.Model;
+    map = map || {};
     $element = getElement($element);
+    var obj = objectFromElement($element, map);
+    return new Model(obj);
 }
 
 exports.modelFromElement = modelFromElement;
+exports.objectFromElement = objectFromElement;
 
 });
--- tests/spec/built-ui-helpers-dom.js
+++ tests/spec/built-ui-helpers-dom.js
@@ -1,5 +1,5 @@
 define(function(require, exports, module) {
-var modelFromElement = require('built/ui/helpers/dom').modelFromElement;
+var helpers = require('built/ui/helpers/dom');
 // Imports
 
 
@@ -17,14 +17,33 @@ describe('ui dom helpers', function() {
     });
 
     // Helpers
-    var $select = $('#jasmine-select');
-    var $option = $select.find('option').eq(0);
-
 
 
     // Test Suite
-    it('converts an option to a model', function(){
+    it('converts an option to an object', function(){
+        var $element = $('option').eq(0);
+        var output = helpers.objectFromElement($element);
+        expect(output).toEqual({test: "talk", value: "fooval", content: "fooopt"});
+    });
 
+    it('converts option to object using map', function(){
+        var $element = $('option').eq(0);
+        var output = helpers.objectFromElement($element, {'content':'option'});
+        expect(output).toEqual({test: "talk", value: "fooval", option: "fooopt"});
+    });
+
+    it('converts an option to a Model', function(){
+        var $element = $('option').eq(0);
+        var output = helpers.modelFromElement($element);
+        var outputModel = new Backbone.Model({test: "talk", value: "fooval", content: "fooopt"});
+        expect(output.toJSON()).toEqual(outputModel.toJSON());
+    });
+
+    it('converts option to Model using map', function(){
+        var $element = $('option').eq(0);
+        var output = helpers.modelFromElement($element, null, {'content':'option'});
+        var outputModel = new Backbone.Model({test: "talk", value: "fooval", option: "fooopt"});
+        expect(output.toJSON()).toEqual(outputModel.toJSON());
     });
 
 
