a688212cbf5641c489c9719092c36f39028a8c59
dinopetrone <dinopetrone@gmail.com>
2013-11-21 10:30:16 -0800

    cleaning up and removing bad references to .data(auf)

--- built/ui/controls/input-select.js
+++ built/ui/controls/input-select.js
@@ -1,10 +1,11 @@
 define(function (require, exports, module) {
 
-var _                  = require('underscore');
-var Marionette         = require('marionette');
-var InputSelect        = require('built/core/controls/forms/input-select').InputSelect;
-var focus              = require('built/core/events/focus');
-var events             = require('built/core/events/event');
+var _            = require('underscore');
+var Marionette   = require('marionette');
+var InputSelect  = require('built/core/controls/forms/input-select').InputSelect;
+var focus        = require('built/core/events/focus');
+var events       = require('built/core/events/event');
+var getElementId = require('built/core/utils/helpers').getElementId;
 
 var InputSelectMarionette = InputSelect.extend({
 
@@ -30,14 +31,14 @@ var InputSelectMarionette = InputSelect.extend({
         this.setElements($(elements));
         var marionetteDict = this.marionetteDict = {};
         _.each(views, function(each){
-            var key = each.$el.data('auf-id');
+            var key = getElementId(each.$el);
             marionetteDict[key] = each;
             elements.push(each.$el[0]);
         });
     },
 
     _triggerEventOnViewForElement : function(event, $element){
-        var key = $element.data('auf-id');
+        var key = getElementId($element);
         var itemView = this.marionetteDict[key];
         if(itemView){
             itemView.trigger(event);
