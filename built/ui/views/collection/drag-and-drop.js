719949a50925370ba925cb1acd9f2f23fd2f7127
Adam Venturella <aventurella@gmail.com>
2013-12-16 04:49:57 -0800

    Drag Drop List
    Fix bug where elements removed from a DragAndDropCollectionView through
    non-drag means were being improperly cleaned up.

--- built/ui/views/collection/drag-and-drop.js
+++ built/ui/views/collection/drag-and-drop.js
@@ -2,12 +2,13 @@ define(function (require, exports, module) {
 
 var marionette = require('marionette');
 var getElementId = require('built/core/utils/helpers').getElementId;
+var registerElement = require('built/core/utils/helpers').registerElement;
 var DragDropList = require('built/core/controls/dragdrop/list').DragDropList;
 
 
 var DragAndDropCollectionView =  marionette.CollectionView.extend({
     initialize: function(options){
-        options = _.extend({dataType:'com.built.data'}, options);
+        options = _.extend({dataType:'com.built.generic'}, options);
 
         _.bindAll(this,
                 'getDragImage',
@@ -62,7 +63,7 @@ var DragAndDropCollectionView =  marionette.CollectionView.extend({
     getDragDataForElement: function($el){
         var view = this.getViewForEl($el);
         var model = view.model;
-        this.collection.remove(model,{silent:true});
+        this.collection.remove(model, {silent: true});
         return this.serializeModel(model);
     },
 
@@ -78,20 +79,54 @@ var DragAndDropCollectionView =  marionette.CollectionView.extend({
         var model = this.deserializeModel(responder.getData());
         var position = this.dragDropList._placeholderIndex;
         this.dragDropList.removePlaceholder();
-        this.collection.add(model,{at:position});
+        this.collection.add(model, {at: position});
     },
 
     draggingEndedRestoreElementAtPosition: function(position, $el){
         var model = this.getViewForEl($el).model.toJSON();
-        this.collection.add(model,{at:position});
+        this.collection.add(model, {at: position});
     },
 
     appendHtml: function(collectionView, itemView, index){
+        // insertDragElement will register the element for
+        // BUILT and add it to the DragResponder.
         this.dragDropList.insertDragElement(index, itemView.$el);
     },
+
+    removeChildView: function(view){
+        // Will not be called on a drag removal
+        // The drag removal operation calls
+        // collection.remove() with the {silent:true} option.
+        //
+        // During dragging, this same logic is basically called
+        // in dragDropList.dragResponderDraggingStarted. During a
+        // dragging operation however, we don't know if the move will
+        // actually happen until the user drops. Remember, this is not
+        // called during a drag-drop operation, so if this is called
+        // it means the user willfully removed a child, probably
+        // using collection.remove. In any case the removal is
+        // definitive, so we have to cleanup.
+
+        var dragDropList = this.dragDropList;
+        var manager = dragDropList.listManager;
+        var list = manager.getArray();
+        var index = list.indexOf(view.$el[0]);
+
+        if(index == -1) return;
+
+        var item = manager.removeObjectAt(index);
+
+        dragDropList.dragResponder.removeElement(view.$el);
+
+        // we need to do the cleanup before super(). Once super() runs,
+        // the element will be destroyed.
+        marionette.CollectionView.prototype.removeChildView.call(this, view);
+
+    }
 
 });
 
 exports.DragAndDropCollectionView = DragAndDropCollectionView;
 
 });
+
