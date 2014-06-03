a2f4fbfb13576fd1626a0912d0c8f17ee1e8e183
Jason Kinnard <Jason@jasonkinnard.local>
2014-06-02 23:09:33 -0700

    initial commit

---
+++ src/static/js/common.js
@@ -0,0 +1,77 @@
+require.config({
+  baseUrl: 'static/js',
+
+  paths : {
+    'marionette': 'vendor/backbone/marionette',
+  },
+
+   packages: [
+
+        {
+            location: 'app',
+            name: 'app'
+        },
+
+        {
+            location: 'vendor/jquery',
+            name: 'jquery',
+            main:'jquery'
+        },
+
+        {
+            location: 'vendor/backbone',
+            name: 'backbone',
+            main:'backbone'
+        },
+
+        {
+            location: 'vendor/built',
+            name: 'built'
+        },
+
+        {
+            location: 'vendor/require/hbs',
+            name: 'hbs',
+            main:'hbs'
+        }
+    ],
+
+    map: {
+        '*': {
+            'underscore': 'vendor/underscore/lodash',
+            'handlebars': 'hbs/handlebars',
+        },
+    },
+
+  hbs: {
+        templateExtension : 'html',
+        // if disableI18n is `true` it won't load locales and the i18n helper
+        // won't work as well.
+        disableI18n : true,
+        helperDirectory: 'app/shared/hbs'
+  },
+
+  shim : {
+
+    'backbone': {
+        'deps': ['jquery', 'underscore'],
+        'exports': 'Backbone'
+    },
+
+    'backbone/stickit' : {
+      'deps' : ['backbone'],
+      'exports' : 'Stickit'
+    },
+
+    'jquery/mockjax': {
+        'deps': ['jquery'],
+        'exports': 'jquery'
+    },
+
+    'marionette': {
+        'deps': ['jquery', 'underscore', 'backbone'],
+        'exports': 'Marionette'
+    }
+  }
+
+});
