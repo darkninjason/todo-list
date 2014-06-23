321868f9aa70f7e5d9c14a7e71485c981737059d
Jason Kinnard <jkinnard@blitzagency.com>
2014-06-23 14:08:08 -0700

    a good first pass at the challenge.  going to continue reworking code/attempt extra credit

--- src/static/js/app/app.js
+++ src/static/js/app/app.js
@@ -10,7 +10,7 @@ define(function(require, exports, module) {
 
     app.addInitializer(function() {
         Backbone.history.start({
-            pushState: true
+            pushState: false
         });
     });
 
